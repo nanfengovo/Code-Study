e9042ac0ffc04d9300d45371ff29112bc19a8c46
nanfengovo <nanfengovo@gmail.com>
2025-12-22 19:22:37 +0800

    日志1

--- Demo/async await/LoggingDemo1/Program.cs
+++ Demo/async await/LoggingDemo1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace LoggingDemo1
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ServiceCollection service = new ServiceCollection();
+            service.AddLogging(logBuilder =>
+            {
+                logBuilder.AddConsole();
+            });
+            service.AddScoped<Test>();
+            using (var sp = service.BuildServiceProvider())
+            {
+                var tst = sp.GetRequiredService<Test>();
+                tst.Test1();
+            }
+
+
         }
     }
 }
---
+++ Demo/async await/LoggingDemo1/Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingDemo1
+{
+    internal class Test
+    {
+        private ILogger<Test> _logger;
+
+        public Test(ILogger<Test> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Test1()
+        {
+            _logger.LogDebug("开始执行数据库同步");
+            _logger.LogInformation("开始执行数据库同步");
+            _logger.LogWarning("开始执行数据库同步");
+            _logger.LogError("开始执行数据库同步");
+        }
+    }
+}
