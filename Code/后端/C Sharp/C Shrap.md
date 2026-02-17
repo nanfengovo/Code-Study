31994c7692c5bef317a9f94a939da66082630d95
南枫ovo <nanfengovo@gmail.com>
2026-02-17 17:09:20 +0800

    DEMO

--- Code/后端/C Sharp/C Shrap.md
+++ Code/后端/C Sharp/C Shrap.md
@@ -32,3 +32,5 @@ https://www.bilibili.com/video/BV1wx411K7rb/?spm_id_from=333.337.search-card.all
 * 类
 * 方法名
 * 命名空间
+# 详解类型，变量与对象
+![[Pasted image 20260214135102.png]]
---
+++ Demo/HelloWorld/StackOverFlowDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StackOverFlowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BadGuy badGuy = new BadGuy();
+            badGuy.BadMethod();
+        }
+    }
+
+    class BadGuy
+    {
+        public void BadMethod()
+        {
+            //var i = 100;
+            BadMethod();
+        }
+    }
+}
